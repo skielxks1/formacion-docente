02199083edf16d4fe83670faabe695185406e9e7
Ezequiel Chicas <ezequielchicas@hotmail.com>
2026-04-15 19:49:44 -0600

    Gestiona entradas y salidas

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+#Trabajando con git
+print("Hello World")
+#Este es un mensaje de prueba para git
+nombre=input("Ingrese su nombre: ")
+print("Hola", nombre)
+edad=input("Ingrese su edad: ")
+print("Usted tiene", edad, "años")
+print("Bienvenido", nombre, "de", edad, "años")
