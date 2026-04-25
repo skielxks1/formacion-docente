9c3508879411c9fecba1145052120c50d26108ce
Ezequiel chicas <ezequielchicas@hotmail.com>
2026-04-24 18:37:39 -0600

    Agregar formas de agregar variables de texto

--- strings.js
+++ strings.js
@@ -2,3 +2,4 @@ let nombre = 'Ezequiel'
 let nacionalidad = 'Salvadoreño'
 
 console.log(`Mi nombre es ${nombre} y soy ${nacionalidad}`);
+console.log('Mi nombre es ' + nombre + ' y soy ' + nacionalidad)
