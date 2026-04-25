187083ed512a8a67095289ae49c1479b8eef775d
Ezequiel chicas <ezequielchicas@hotmail.com>
2026-04-24 18:49:54 -0600

    Trabajo en construccion

--- strings.js
+++ strings.js
@@ -3,3 +3,6 @@ let nacionalidad = 'Salvadoreño'
 
 console.log(`Mi nombre es ${nombre} y soy ${nacionalidad}`);
 console.log('Mi nombre es ' + nombre + ' y soy ' + nacionalidad)
+
+//obtener el numero de caracteres de una variable
+console.log(`La variaable nombre tiene ${nombre.length} caracteres`)
