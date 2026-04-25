670fbace8191214124554b5ffa62186dce16a8d5
Ezequiel chicas <ezequielchicas@hotmail.com>
2026-04-24 18:52:59 -0600

    agregar mas funciones

--- strings.js
+++ strings.js
@@ -6,3 +6,16 @@ console.log('Mi nombre es ' + nombre + ' y soy ' + nacionalidad)
 
 //obtener el numero de caracteres de una variable
 console.log(`La variaable nombre tiene ${nombre.length} caracteres`)
+
+//convertir una variable a mayúsculas
+console.log(nombre.toUpperCase())
+
+//convertir una variable a minúsculas
+console.log(nombre.toLowerCase())
+
+//convertir una variable a mayúsculas
+console.log(nombre.toUpperCase())
+
+//convertir una variable a minúsculas
+console.log(nombre.toLowerCase())
+
