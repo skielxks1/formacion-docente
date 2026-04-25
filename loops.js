79081f510e37d1d3db8737508d18180951cee5a9
Ezequiel chicas <ezequielchicas@hotmail.com>
2026-04-24 19:54:43 -0600

    feat: add loops.js file demonstrating for, while, and do-while loop implementations

---
+++ loops.js
@@ -0,0 +1,16 @@
+// ciclo for
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+// ciclo while
+let i = 0
+while (i < 10) {
+    console.log(i)
+    i++
+}
+// ciclo do while
+let j = 0
+do {
+    console.log(j)
+    j++
+} while (j < 10)
