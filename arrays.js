49c5d5cd863c93211c35fe8e3eeedcf532fe134b
Ezequiel chicas <ezequielchicas@hotmail.com>
2026-04-27 19:34:27 -0600

    Trabajar con arreglos

---
+++ arrays.js
@@ -0,0 +1,14 @@
+let frutas = ["manzana", "pera", "uva", "naranja", "sandia"];
+
+frutas.forEach(function (fruta) {
+    console.log(`La fruta es ${fruta}`);
+});
+
+console.log(`La primera fruta es ${frutas[0]}`)
+console.log(`Cantidad de frutas: ${frutas.length}`)
+
+frutas.forEach(frutas => {
+    console.log(`La fruta es ${frutas}`);
+}
+
+)
---
+++ arraysFunctions.js
@@ -0,0 +1,19 @@
+let frutas = ["manzana", "pera", "uva", "naranja", "sandia"];
+
+frutas.push("piña"); //agrega elemenos al final
+console.log(frutas);
+
+
+//eleimindando el ultimo elemento
+frutas.pop();
+console.log(frutas);
+
+//agregar un elemento al inicio
+frutas.unshift("kiwi");
+console.log(frutas);
+
+//eliminar el primer elemento   
+frutas.shift();
+console.log(frutas);
+
+
